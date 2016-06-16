edf71f1e90fdbea7acf96230a40f245742c8a1c0
Steve Klabnik <steve@steveklabnik.com>
2016-06-16 11:05:59 -0400

    extract BitStorage and BitVector into modules

---
+++ src/bit_storage.rs
@@ -0,0 +1,31 @@
+use std::ops::{BitAnd,BitAndAssign,BitOr,BitOrAssign,BitXor,BitXorAssign,Not,Shl,ShlAssign,Shr,ShrAssign};
+use num::{One,Zero,Unsigned,NumCast};
+
+pub trait BitStorage: Sized +
+    BitAnd<Self, Output = Self> +
+    BitAndAssign<Self> +
+    BitOr<Self, Output = Self> +
+    BitOrAssign<Self> +
+    BitXor<Self, Output = Self> +
+    BitXorAssign<Self> +
+    Not<Output = Self> +
+    Shl<Self, Output = Self> +
+    ShlAssign<Self> +
+    Shr<Self, Output = Self> +
+    ShrAssign<Self> +
+    Eq + Zero + One + Unsigned + NumCast + Copy {}
+
+impl<S> BitStorage for S where S: Sized +
+    BitAnd<S, Output = S> +
+    BitAndAssign<S> +
+    BitOr<S, Output = S> +
+    BitOrAssign<S> +
+    BitXor<S, Output = S> +
+    BitXorAssign<S> +
+    Not<Output = S> +
+    Shl<S, Output = S> +
+    ShlAssign<S> +
+    Shr<S, Output = S> +
+    ShrAssign<S> +
+    Eq + Zero + One + Unsigned + NumCast + Copy {}
+
